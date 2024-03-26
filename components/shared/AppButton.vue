816effc3d96a7a5452cc5c8fbff2ed8f195d6901
Hope <hagyaba@infonaligy.com>
2024-03-26 22:53:50 +0000

    added app button component

---
+++ components/shared/AppButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  variant?: 'primary' | 'secondary'
+  label?: string
+}>()
+</script>
+
+<template>
+  <button
+    class="inline-flex items-center gap-2 rounded-[10px] text-gray-3 px-10 py-4 font-medium"
+    :class="{
+      'bg-primary': !variant || variant === 'primary',
+      'bg-secondary': variant === 'secondary'
+    }"
+  >
+    <slot>{{ label }}</slot>
+  </button>
+</template>
