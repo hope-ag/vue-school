cb35ecde74bef7795edd8df2ebd7ac88643b6e4f
Hope <hagyaba@infonaligy.com>
2024-03-26 20:34:58 +0000

    added nuxt-image, icon, and google-font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    '@nuxt/image',
+    'nuxt-icon',
+    '@nuxtjs/google-fonts'
+  ],
   tailwindcss: {
     configPath: './tailwind.config.ts'
   },
-  eslint: {}
+  eslint: {},
+  image: {},
+  googleFonts: {
+    families: {
+      Rubik: [300, 400, 500, 600, 700, 800, 900]
+    }
+  }
 })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>>{
   theme: {
@@ -10,9 +11,24 @@ export default <Partial<Config>>{
         },
         secondary: '#01F0FD',
         surface: {
-          DEFAULT: '#2B2F44',
-          dark: '#212435'
+          DEFAULT: '#212435',
+          light: '#2B2F44'
+        },
+        tertiary: {
+          violet: '#4F08E6',
+          orange: '#FF4975'
+        },
+        gray: {
+          DEFAULT: '#F3F5FF',
+          '1': '#797B88',
+          '2': '#737B98'
         }
+      },
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans]
+      },
+      container: {
+        center: true
       }
     }
   }
