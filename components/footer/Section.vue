bbaccdba7cdbbbf338bc6f612dfdd65861e612ba
Hope <hagyaba@infonaligy.com>
2024-03-29 01:07:23 +0000

    final footer updates

--- components/footer/Section.vue
+++ components/footer/Section.vue
@@ -1,29 +1,41 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="mt-24 sm:mt-40 lg:mt-52">
+  <footer class="mt-24 sm:mt-40 lg:mt-52 pb-12 sm:pb-20">
     <div class="content-wrapper">
-      <div class="grid lg:justify-between md:grid-cols-2 md:gap-16 container p-4">
+      <div class="grid lg:justify-between lg:grid-cols-2 lg:gap-16 container p-4">
         <div
-          class="flex flex-col items-center md:items-start gap-8 sm:gap-10 lg:gap-[85px] lg:max-w-[470px]"
+          class="flex flex-col items-center lg:items-start gap-8 sm:gap-10 lg:gap-[85px] lg:max-w-[470px]"
         >
           <NuxtImg src="/logo.svg" alt="Vue school logo" class="h-[88px]" />
-          <p class="text-white">
+          <p class="text-white text-center lg:text-left">
             Thanks to fine people like you, Vue School can proudly sponsor Evan and the
             future of Vue.js by being a Platinum Patreon
           </p>
           <FooterSocials />
         </div>
-        <div class="grid sm:grid-cols-2">
-          <div class="lg:space-y-[50px]">
+        <div
+          class="grid lg:grid-cols-2 items-center sm:items-start text-center sm:text-left mt-12 sm:mt-16 lg:mt-0"
+        >
+          <div
+            class="lg:space-y-[50px] grid gap-8 grid-cols-1 sm:grid-cols-3 justify-between lg:block"
+          >
             <FooterLinkGroup v-bind="footerProductLinks" />
-            <FooterLinkGroup v-bind="footerPricingLinks" />
+            <FooterLinkGroup
+              v-bind="isDesktop ? footerPricingLinks : footerCorporateLinks"
+            />
             <FooterLinkGroup v-bind="footerConnectLinks" />
           </div>
-          <div class="lg:space-y-[50px]">
+          <div
+            class="lg:space-y-[50px] grid gap-8 grid-cols-1 sm:grid-cols-3 justify-between lg:block mt-8 lg:mt-0"
+          >
             <FooterLinkGroup v-bind="footerEventLinks" />
-            <FooterLinkGroup v-bind="footerCorporateLinks" />
-            <FooterLinkGroup v-bind="footerResourceLinks" />
+            <FooterLinkGroup
+              v-bind="isDesktop ? footerCorporateLinks : footerResourceLinks"
+            />
+            <FooterLinkGroup
+              v-bind="isDesktop ? footerResourceLinks : footerPricingLinks"
+            />
           </div>
         </div>
       </div>
--- components/footer/Socials.vue
+++ components/footer/Socials.vue
@@ -1,36 +1,36 @@
 <script setup lang="ts">
 const iconLinkClass =
-  'grid place-content-center bg-surface-light h-[60px] w-[60px] rounded-full'
+  'grid place-content-center bg-surface-light h-[60px] w-[60px] rounded-full text-gray-1 duration-300'
 </script>
 
 <template>
-  <div aria-label="social links">
+  <div aria-label="social links" class="text-center lg:text-left">
     <h2 class="mb-5 uppercase text-gray-2 font-medium text-[22px]">
       FOLLOW US IN SOCIAL MEDIA
     </h2>
-    <ul class="flex items-center justify-between gap-2 sm:gap-8 md:gap-10">
+    <ul class="flex items-center justify-between gap-4 sm:gap-8 md:gap-10">
       <li>
-        <NuxtLink :class="iconLinkClass">
+        <NuxtLink :class="iconLinkClass" class="hover:text-[#3b5998]">
           <IconsFacebook />
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink :class="iconLinkClass">
+        <NuxtLink :class="iconLinkClass" class="hover:text-sky-500">
           <IconsTwitter />
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink :class="iconLinkClass">
+        <NuxtLink :class="iconLinkClass" class="hover:text-white">
           <IconsGithub />
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink :class="iconLinkClass">
+        <NuxtLink :class="iconLinkClass" class="hover:text-[#0e76a8]">
           <IconsLinkedIn />
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink :class="iconLinkClass">
+        <NuxtLink :class="iconLinkClass" class="hover:text-[#b2071d]">
           <IconsYouTube />
         </NuxtLink>
       </li>
