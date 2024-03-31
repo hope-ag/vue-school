d7c9e66e81ae1a0b8d294997060992710780cee7
Hope <hagyaba@infonaligy.com>
2024-03-31 15:49:54 +0000

    opengraph updates for twitter

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,9 @@ useSeoMeta({
   ogImage: '/og-image.png',
   ogImageType: 'image/png',
   ogTitle: 'Vue School | The #1 source for learning Vue.js from experts',
+  twitterTitle: 'Vue School | The #1 source for learning Vue.js from experts',
+  twitterCard: 'summary_large_image',
+  twitterImage: '/og-image.png',
   ogDescription:
     'Basic to advanced Vue.js training from core team members & industry experts. In-depth lessons with weekly updates on Pinia, Typescript, Vue Use, Laravel, & more',
   twitterDescription:
