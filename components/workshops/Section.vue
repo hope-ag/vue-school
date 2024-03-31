1fdaa157fd9f98b2ec511dfd2691b3465f301cd3
Hope <hagyaba@infonaligy.com>
2024-03-31 14:28:35 +0000

    added buttons fro scrolling workshops section (mouse users)

--- components/workshops/Section.vue
+++ components/workshops/Section.vue
@@ -16,6 +16,59 @@ const workshope = [
       'Take a deep dive into the new and exciting features of Vue 3. Learn about Portals, Suspense, Composition API, Fragments, and much much more...'
   }
 ]
+
+const scrollButtonClass =
+  'inline-grid place-content-center h-12 w-12 rounded-full bg-gray-2/20 hover:bg-gray-2/40 active:bg-gray-2/10 duration-200 disabled:cursor-not-allowed'
+
+const scrollWrapper = ref<HTMLElement>()
+const cardsWrapper = ref<HTMLElement>()
+const canScrollLeft = ref(false)
+const canScrollRight = ref(false)
+const defaultScrollFactor = 400
+
+const { width } = useElementSize(cardsWrapper)
+
+function calcCanScrollLeft() {
+  if (!scrollWrapper.value) return false
+  return scrollWrapper.value.scrollLeft > 0
+}
+
+function calcCanScrollRight() {
+  if (!scrollWrapper.value) return false
+  return scrollWrapper.value.scrollLeft <= width.value - scrollWrapper.value.clientWidth
+}
+
+function updateButtons() {
+  canScrollLeft.value = calcCanScrollLeft()
+  canScrollRight.value = calcCanScrollRight()
+}
+
+function doScroll(type: 'left' | 'right') {
+  const scrollRatio = isDesktop.value ? 0.5 : 1 / 3
+  const scrollFactor = width.value ? width.value * scrollRatio : defaultScrollFactor
+  if (!scrollWrapper.value) return
+  if (type === 'left') {
+    scrollWrapper.value.scrollLeft =
+      scrollWrapper.value.scrollLeft > scrollFactor
+        ? scrollWrapper.value.scrollLeft - scrollFactor
+        : 0
+  } else {
+    scrollWrapper.value.scrollLeft += scrollFactor
+  }
+}
+
+onMounted(() => {
+  if (!scrollWrapper.value) return
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        updateButtons()
+        observer.unobserve(entry.target)
+      }
+    })
+  })
+  observer.observe(scrollWrapper.value)
+})
 </script>
 
 <template>
@@ -32,10 +85,37 @@ const workshope = [
     </div>
 
     <div class="relative group">
-      <div class="overflow-x-scroll scroll">
+      <div class="absolute flex items-center gap-6 right-4 bottom-full">
+        <button
+          :disabled="!canScrollLeft"
+          :class="[scrollButtonClass, canScrollLeft ? '' : 'opacity-20']"
+          @click="doScroll('left')"
+        >
+          <span class="sr-only">Scroll left</span>
+          <Icon name="ion:chevron-left" />
+        </button>
+        <button
+          :disabled="!canScrollRight"
+          :class="[scrollButtonClass, canScrollRight ? '' : 'opacity-20']"
+          @click="doScroll('right')"
+        >
+          <span class="sr-only">Scroll right</span>
+          <Icon name="ion:chevron-right" />
+        </button>
+      </div>
+      <div
+        ref="scrollWrapper"
+        class="overflow-x-scroll scroll-smooth scrollbar-none"
+        aria-orientation="horizontal"
+        @scroll="updateButtons"
+        @mouseover="updateButtons"
+      >
         <div class="content-wrapper pb-2">
           <div class="container py-4">
-            <div class="workshop-cards flex w-max gap-4 sm:gap-6 pr-4 sm:pr-6">
+            <div
+              ref="cardsWrapper"
+              class="workshop-cards flex w-max gap-4 sm:gap-6 pr-4 sm:pr-6"
+            >
               <WorkshopsCard
                 v-for="workshop in workshope"
                 :key="workshop.title"
